9db867ee675f23b3454dc8beefa2b63c14b49727
Elizabeth Pedraza <eli456pg@gmail.com>
2023-06-16 04:03:48 -0600

    release

--- flores/schema.py
+++ flores/schema.py
@@ -1,12 +1,16 @@
 import graphene
-
 import flowers.schema
+import users.schema
+import graphql_jwt
+import myapp.schema
 
-class Query(flowers.schema.Query, graphene.ObjectType) :
+
+class Query(myapp.schema.Query, users.schema.Query,flowers.schema.Query, graphene.ObjectType) :
     pass
 
-class Mutation(flowers.schema.Mutation, graphene.ObjectType):
-    pass
-
+class Mutation(myapp.schema.Mutation,users.schema.Mutation, flowers.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)

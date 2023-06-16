fa7bfa8785d17d1ed4e5e80fce4110a1ed5d8aad
Elizabeth Pedraza <eli456pg@gmail.com>
2023-06-16 03:35:53 -0600

    update release

--- settings/production.py
+++ settings/production.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(_file_).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'flowers',
+    'flores',
+    'myapp',
     'corsheaders',
     
 ]
